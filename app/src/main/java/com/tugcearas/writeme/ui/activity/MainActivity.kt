d8006957c68320b618e93195cd54c676699272d8
TugceAras <aras.tugcee@gmail.com>
2023-01-29 10:26:05 +0300

    home xml updated

--- app/src/main/java/com/tugcearas/writeme/ui/activity/MainActivity.kt
+++ app/src/main/java/com/tugcearas/writeme/ui/activity/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#474747")))
-
     }
 }
 
